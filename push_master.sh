53cc1928994dd0ff82d3fccd84e86b2c8b7a7f2f
dingxl <dingxl18@qq.com>
2021-07-17 15:44:31 +0800

    update

---
+++ push_master.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# git@gitee.com:xsheep-tech/docker-image.git
+# git@github.com:rickding/docker-image.git
+
+git checkout master
+
+git push origin master
+git push gitee master
+git push github master
